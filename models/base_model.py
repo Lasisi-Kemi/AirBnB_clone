79374327339db203e3061ba448f1691bb798ea8c
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-08-30 17:43:02 +0000

    added solution to task 3 BaseModel and initial file setup

---
+++ models/base_model.py
@@ -0,0 +1,55 @@
+"""
+   The base_model module
+
+   Defines a single BaseModel class for initiating the projects objects
+
+"""
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+
+    """
+       The base model class
+
+       defines all common attributes/methods for other classes
+
+    """
+
+    def __init__(self, name=None, my_number=None):
+        self.name = name
+        self.my_number = my_number
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        """
+            updates the public instance attribute updated_at
+
+            with the current datetime
+
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+             returns a dictionary containing all keys/values
+
+             of __dict__ of the instance
+
+        """
+        dictionary = self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        created_at = dictionary["created_at"]
+        updated_at = dictionary["updated_at"]
+        dictionary["created_at"] = created_at.isoformat()
+        dictionary["updated_at"] = updated_at.isoformat()
+
+        return dictionary
